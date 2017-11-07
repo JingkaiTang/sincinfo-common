a88b73da7bcc911afa89636c2f4235309b29f65f
Jingkai Tang <jingkaitang@qq.com>
2017-11-07 16:45:47 +0800

    scoped some styles

--- src/components/main_components/sidebarMenu.vue
+++ src/components/main_components/sidebarMenu.vue
@@ -1,4 +1,4 @@
-<style lang="less">
+<style lang="less" scoped>
   @import './menu.less';
 </style>
 
--- src/components/main_components/tagsPageOpened.vue
+++ src/components/main_components/tagsPageOpened.vue
@@ -1,4 +1,4 @@
-<style lang="less">
+<style lang="less" scoped>
   @import '../main.less';
 </style>
 
